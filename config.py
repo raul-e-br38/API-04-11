fbdb7453877dd07ef27158f495cec69cdebc5e7b
raul-e-br38 <167575443+raul-e-br38@users.noreply.github.com>
2025-11-11 15:15:35 -0300

    .

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 SECRET_KEY = 'CHAVE_SECRETAS_SECRETISSIMA'
 DEBUG = True
 DB_HOST = 'localhost'
-DB_NAME = r'C:\Users\Aluno\Desktop\AUTOPRIME.FDB'
+DB_NAME = r'C:\Users\Aluno\Desktop\API-04-11\AUTOPRIME.FDB'
 DB_USER = 'sysdba'
 DB_PASSWORD = 'sysdba'
 
